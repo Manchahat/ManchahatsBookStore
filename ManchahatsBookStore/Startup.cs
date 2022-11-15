f0c7111c5e2e1c6dcb4f1b08e21eb2ca2e392522
W0802991 <W0802991@ADSOL0214Z-05>
2022-11-15 11:25:03 -0500

    updated

--- ManchahatsBookStore/Startup.cs
+++ ManchahatsBookStore/Startup.cs
@@ -64,9 +64,11 @@ namespace ManchahatsBookStore
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
+                    name: "Areas",
+                    areaName: "Costumer",
                     // added route for scaffolding ReadMe.txt
-                    pattern: "{area=customer}/{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=customer}/{controller=Home}/{action=Index}/{id?}") ;
+
                 endpoints.MapRazorPages();
             });
         }
